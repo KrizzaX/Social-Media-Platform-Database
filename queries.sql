37d132a63b57d91da7b30d93be97ea1ac42a9bbd
Claudio Hernandez <claudioherza@gmail.com>
2026-05-24 12:38:13 -0400

    queries 4,5,6

--- queries.sql
+++ queries.sql
@@ -20,3 +20,35 @@ WHERE name = 'Jane Smith';
 UPDATE message
 SET read_status = 'read'
 WHERE message_id = 3;
+
+-- Query 4 Count the number of likes on a particular post
+SELECT COUNT(user_id) AS Like_Count
+FROM Likes
+WHERE post_id = X;
+
+-- Query 5 List all friends of a specific user.
+SELECT u.*
+FROM Users u
+JOIN FriendShips f
+    ON u.user_id = f.addressee_id
+WHERE f.requester_id = X
+  AND f.status = 'accepted'
+
+UNION
+
+SELECT u.*
+FROM Users u
+JOIN FriendShips f
+    ON u.user_id = f.requester_id
+WHERE f.addressee_id = X
+  AND f.status = 'accepted';
+
+
+-- Query 6 Find all unread messages for a user.
+SELECT m.*
+FROM Messages m
+JOIN Chat_Participants cp
+    ON m.chat_id = cp.chat_id
+WHERE cp.user_id = X
+  AND m.sender_id <> X
+  AND m.status = 'Unread';
