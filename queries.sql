32f40ef45fd7bdc47e41d6e994e4927a9716223f
Kris Xhangolli <148934114+KrizzaX@users.noreply.github.com>
2026-08-16 22:55:12 -0400

    Fix queries to match database schema

--- queries.sql
+++ queries.sql
@@ -1,86 +1,87 @@
--- Query 1 Retrieve profile information for a specific user by username.
+-- Query 1: Retrieve profile information for a specific user by username
 SELECT *
-FROM Users
-WHERE Username = 'X';
+FROM user
+WHERE username = 'X';
 
--- Query 2 List all posts created by a particular user.
+
+-- Query 2: List all posts created by a particular user
 SELECT p.*
-FROM Posts p
-JOIN Users u
-    ON p.User_ID = u.User_ID
-WHERE u.Username = 'X';
+FROM post p
+JOIN user u
+    ON p.user_id = u.user_id
+WHERE u.username = 'X';
 
--- Query 3 Get all comments on a specific post.
+
+-- Query 3: Get all comments on a specific post
 SELECT c.*
-FROM Comments c
-WHERE c.Post_ID = X;
+FROM comment c
+WHERE c.post_id = X;
 
--- Query 4 Count the number of likes on a particular post
-SELECT COUNT(user_id) AS Like_Count
-FROM Likes
+
+-- Query 4: Count the number of likes on a particular post
+SELECT COUNT(*) AS like_count
+FROM likes
 WHERE post_id = X;
 
--- Query 5 List all friends of a specific user.
+
+-- Query 5: List all friends of a specific user
 SELECT u.*
-FROM Users u
-JOIN FriendShips f
-    ON u.user_id = f.addressee_id
-WHERE f.requester_id = X
-  AND f.status = 'accepted'
+FROM user u
+JOIN friendship_status f
+    ON u.user_id = f.receiver_user_id
+WHERE f.sender_user_id = X
+  AND f.status = 'Friends'
 
 UNION
 
 SELECT u.*
-FROM Users u
-JOIN FriendShips f
-    ON u.user_id = f.requester_id
-WHERE f.addressee_id = X
-  AND f.status = 'accepted';
+FROM user u
+JOIN friendship_status f
+    ON u.user_id = f.sender_user_id
+WHERE f.receiver_user_id = X
+  AND f.status = 'Friends';
 
 
--- Query 6 Find all unread messages for a user.
+-- Query 6: Find all messages received by a specific user
 SELECT m.*
-FROM Messages m
-JOIN Chat_Participants cp
-    ON m.chat_id = cp.chat_id
-WHERE cp.user_id = X
-  AND m.sender_id <> X
-  AND m.status = 'Unread';
+FROM message m
+WHERE m.receiver_user_id = X;
 
--- Query 7 Retrieve all notifications for a specific user.
+
+-- Query 7: Retrieve all notifications for a specific user
 SELECT n.*
 FROM notification n
 JOIN user u
     ON n.user_id = u.user_id
 WHERE u.username = 'X';
 
--- Query 8 List all members of a particular group
 
-SELECT u.user_id
+-- Query 8: List all members of a particular group
+SELECT u.*
 FROM user u
-JOIN group_membership gm 
-  ON gm.user_id = u.user_id
-JOIN group g
-  ON g.id = gm.id
-WHERE group_name = 'X';
+JOIN group_membership gm
+    ON u.user_id = gm.user_id
+JOIN groups g
+    ON gm.group_id = g.group_id
+WHERE g.group_name = 'X';
 
--- Query 9 Find the most popular posts with a certain minimum number of likes.
-SELECT 
-    p.post_id, 
-    p.content, 
+
+-- Query 9: Find posts with at least a certain number of likes
+SELECT
+    p.post_id,
+    p.content,
     COUNT(l.like_id) AS total_likes
 FROM post p
-JOIN likes l 
+JOIN likes l
     ON p.post_id = l.post_id
-GROUP BY 
-    p.post_id, 
+GROUP BY
+    p.post_id,
     p.content
-HAVING 
-    COUNT(l.like_id) >= 'X'
-ORDER BY 
-    total_likes DESC;
-  
--- Query 10 Retrieve all posts tagged with a specific location.
+HAVING COUNT(l.like_id) >= X
+ORDER BY total_likes DESC;
+
+
+-- Query 10: Retrieve all posts with a specific visibility setting
 SELECT *
 FROM post
-WHERE location = 'X';
+WHERE visibility = 'X';
